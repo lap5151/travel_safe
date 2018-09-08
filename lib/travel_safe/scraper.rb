ee61dcef383e7e9f64bd52a67fb9d3239e839135
Lauren Pardee <lap5151@gmail.com>
2018-09-08 19:30:22 +0000

    working on scraper method

--- lib/travel_safe/scraper.rb
+++ lib/travel_safe/scraper.rb
@@ -6,28 +6,16 @@ class TravelSafe::Scraper
 
   def self.scrape_page(url= "https://travel.state.gov/content/travel/en/traveladvisories/traveladvisories.html")
     countries = []
+    scrape_results = []
     website = Nokogiri::HTML(open(url))
-    website.css("div.table-data.data-date").each do |info|
-        #info.css("tr").children.each do |row|
-          info.css("tr").children each do |row|
+    website.css("div.table-data.data-date").each.with_index(1) do |info, i|
+        scrape_results << info.css("tr")[i]
+          scrape_results.each do |row|
             url_name = row.css("a").attribute("href").value
             full_url = "https://travel.state.gov" + url_name
             country_name = row.css("a").attribute("title").value
-          #row.css("td").children do |result|
-        #name = result.css("td")[0]
-        #country = name.split(/.travel advisory$/)
-        #advisory_level = result.css("td")[1].text
-        #url = result.css("td a").attribute("href").text
-      #  country_url = "https://travel.state.gov" + url
-        #TravelSafe::Country.new(name,advisory_level,country_url)
-        #countries << name
-                      binding.pry
-            #TravelSafe::Country.new(country)
-        #  country.css("").each do |country_url|
-        #    country.country_url = country_url
-            #binding.pry
-            #self.scrape_country(country_url)
-        #end
+            country = country_name.split(/.travel advisory$/)
+                binding.pry
       end
     end
   end
