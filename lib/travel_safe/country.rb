29d59912a1a0f948c17fcd44487d3e1f4bac26bb
Lauren Pardee <lap5151@gmail.com>
2018-09-11 03:19:12 +0000

    trying to scrape each country url

--- lib/travel_safe/country.rb
+++ lib/travel_safe/country.rb
@@ -1,13 +1,21 @@
 class TravelSafe::Country
 
-attr_accessor :name, :country_url, :advisory_level, :advisory_info, :tourist_visa, :vaccinations, :currency_restrictions_entry, :currency_restrictions_exit, :embassy_address, :embassy_phone, :emergency_phone
+attr_accessor :name, :country_url, :info_url, :advisory_level, :advisory_info, :passport_validity, :blank_passport_pages, :tourist_visa, :vaccinations, :currency_restrictions_entry, :currency_restrictions_exit, :embassy_address, :embassy_phone, :emergency_phone
 
 @@all = []
 
-  def initialize(name,advisory_level,country_url)
+  def initialize(name,advisory_level,country_url,info_url)
     @name = name
     @advisory_level = advisory_level
     @country_url = country_url
+    @info_url = info_url
+    #TravelSafe::Scraper.scrape_country(country_url)
+  #  @passport_validity = passport_validity
+  #  @blank_passport_pages = blank_passport_pages
+  #  @tourist_visa = tourist_visa
+  #  @vaccinations = vaccinations
+  #  @currency_restrictions_entry = currency_restrictions_entry
+  #  @currency_restrictions_exit = currency_restrictions_exit
     @@all << self
   end
 
@@ -18,12 +26,16 @@ attr_accessor :name, :country_url, :advisory_level, :advisory_info, :tourist_vis
   def self.list_all
     countries = []
     @@all.each  do |instance|
-      countries << instance.name
+      countries << instance.info_url
     end
     countries.sort.each.with_index(1) do |country, i|
         puts "#{i}. #{country}"
       end
       countries = []
+  end
+
+  def self.more_info
+    puts "Passport validity: #{@passport_validity}"
   end
 
 end
--- lib/travel_safe/scraper.rb
+++ lib/travel_safe/scraper.rb
@@ -10,37 +10,49 @@ class TravelSafe::Scraper
         end
       end
       i=1
-      while i < scrape_results.count do
+      while i < 190 do#scrape_results.count do
             url_name = scrape_results[i].css("a").attribute("href").value
-            full_url = "https://travel.state.gov" + url_name
+            country_url = "https://travel.state.gov" + url_name
             country_name = scrape_results[i].css("a").attribute("title").value.strip
             country = country_name.split(/.Travel Advisory$/).join
             advisory_level = scrape_results[i].css("td")[1].children.text
-            instance = TravelSafe::Country.new(country,advisory_level,full_url)
+            country_website = Nokogiri::HTML(open(country_url))
+            info_url = country_website.css("div.tsg-rwd-emergency-alert-text a").attribute("href").value
+            if info_url.strip.split("https://travel.state.gov").count == 1
+              info_url = "https://travel.state.gov" + info_url.strip
+            else
+              info_url = info_url.strip
+            end
+            instance = TravelSafe::Country.new(country,advisory_level,country_url,info_url)
             i+=1
             countries << instance
       end
       countries.count
     end
 
-  def scrape_country(country_url = "https://travel.state.gov/content/travel/en/traveladvisories/traveladvisories/burma-travel-advisory.html")
+  def self.scrape_country(country_url = "https://travel.state.gov/content/travel/en/traveladvisories/traveladvisories/japan-travel-advisory.html")
   blocks = []
   country_website = Nokogiri::HTML(open(country_url))
   info_url = country_website.css("div.tsg-rwd-emergency-alert-text a").attribute("href").value
-  safety_info = country_website.css("div.tsg-rwd-emergency-alert-text p")
-  avoid = country_website.css("div.tsg-rwd-emergency-alert-text li")
-  lists = avoid.css("li").text
-  blocks << safety_info.css("p")[0].text
-  safety_info.css("p")[1]
+  if info_url.strip.split("https://travel.state.gov").count == 1
+    info_url = "https://travel.state.gov" + info_url.strip
+  else
+    info_url = info_url.strip
+  end
   info_website = Nokogiri::HTML(open(info_url))
+#  safety_info = country_website.css("div.tsg-rwd-emergency-alert-text p")
+#  avoid = country_website.css("div.tsg-rwd-emergency-alert-text li")
+#  lists = avoid.css("li").text
+#  blocks << safety_info.css("p")[0].text
+  #safety_info.css("p")[1]
   info_block = info_website.css("div.tsg-rwd-qf-box-data")
-  passport_validity = info_block[0].text.strip
-  blank_passport_pages = info_block[1].text.strip
-  tourist_visa = info_block[2].text.strip
-  vaccinations = info_block[3].text.strip
-  currency_restrictions_entry = info_block[4].text.strip
-  currency_restrictions_exit = info_block[5].text.strip
-  safety_block = info_website.css("div.tsg-rwd-accordion-copy")
+  passport_validity = info_block[0].text.strip if info_block[0] != nil
+  blank_passport_pages = info_block[1].text.strip if info_block[1] != nil
+  tourist_visa = info_block[2].text.strip if info_block[2] != nil
+  vaccinations = info_block[3].text.strip if info_block[3] != nil
+  currency_restrictions_entry = info_block[4].text.strip if info_block[4] != nil
+  currency_restrictions_exit = info_block[5].text.strip if info_block[5] != nil
+#  safety_block = info_website.css("div.tsg-rwd-accordion-copy")
   #safety_and_security = safety_block[3].text.strip
     #safety_block[3].xpath("p").each do |block|
       #blocks << block.text
