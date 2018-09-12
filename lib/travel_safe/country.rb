8a9f658b2d10c3ecfa741295e61897dcf5f2cfa7
Lauren Pardee <lap5151@gmail.com>
2018-09-12 00:44:30 +0000

    links and scraper are working

--- lib/travel_safe/country.rb
+++ lib/travel_safe/country.rb
@@ -9,13 +9,6 @@ attr_accessor :name, :country_url, :info_url, :advisory_level, :advisory_info, :
     @advisory_level = advisory_level
     @country_url = country_url
     @info_url = info_url
-    #TravelSafe::Scraper.scrape_country(country_url)
-  #  @passport_validity = passport_validity
-  #  @blank_passport_pages = blank_passport_pages
-  #  @tourist_visa = tourist_visa
-  #  @vaccinations = vaccinations
-  #  @currency_restrictions_entry = currency_restrictions_entry
-  #  @currency_restrictions_exit = currency_restrictions_exit
     @@all << self
   end
 
@@ -26,7 +19,7 @@ attr_accessor :name, :country_url, :info_url, :advisory_level, :advisory_info, :
   def self.list_all
     countries = []
     @@all.each  do |instance|
-      countries << instance.info_url
+      countries << instance.name
     end
     countries.sort.each.with_index(1) do |country, i|
         puts "#{i}. #{country}"
@@ -35,12 +28,12 @@ attr_accessor :name, :country_url, :info_url, :advisory_level, :advisory_info, :
   end
 
   def self.more_info(country)
-    #puts "Passport validity: #{@passport_validity}"
-    #puts "Blank Passport Pages: #{@blank_passport_pages}"
-    #puts "Tourist Visa Required: #{@tourist_visa"
-    #puts "Vaccinations: #{@vaccinations}"
-    #puts "Currency Restrictions for Entry: #{@currency_restrictions_entry}"
-    #puts "Currency Restrictions for Exit: #{@currency_restrictions_exit}"
+    puts "Passport validity: #{country.passport_validity}"
+    puts "Blank Passport Pages: #{country.blank_passport_pages}"
+    puts "Tourist Visa Required: #{country.tourist_visa}"
+    puts "Vaccinations: #{country.vaccinations}"
+    puts "Currency Restrictions for Entry: #{country.currency_restrictions_entry}"
+    puts "Currency Restrictions for Exit: #{country.currency_restrictions_exit}"
     puts "For more information please visit the webiste below: "
     puts "#{country.country_url}"
   end
--- lib/travel_safe/scraper.rb
+++ lib/travel_safe/scraper.rb
@@ -28,7 +28,6 @@ class TravelSafe::Scraper
               info_url = info_url_3["href"]
             else
               info_url = country_website.css("div.tsg-rwd-emergency-alert-text a").attribute("href").value
-              #info_url = "https://travel.state.gov/content/travel/en/traveladvisories/traveladvisories/worldwide-caution.html"
             end
             if info_url.strip.split("https://www.faa.gov").count == 2
               info_url = info_url.strip
@@ -42,22 +41,35 @@ class TravelSafe::Scraper
               info_url = info_url.strip
             end
             instance = TravelSafe::Country.new(country,advisory_level,country_url,info_url)
+            info_website = Nokogiri::HTML(open(info_url))
+          #  safety_info = country_website.css("div.tsg-rwd-emergency-alert-text p")
+          #  avoid = country_website.css("div.tsg-rwd-emergency-alert-text li")
+          #  lists = avoid.css("li").text
+          #  blocks << safety_info.css("p")[0].text
+            #safety_info.css("p")[1]
+            info_block = info_website.css("div.tsg-rwd-qf-box-data")
+            instance.passport_validity = info_block[0].text.strip if info_block[0] != nil
+            instance.blank_passport_pages = info_block[1].text.strip if info_block[1] != nil
+            instance.tourist_visa = info_block[2].text.strip if info_block[2] != nil
+            instance.vaccinations = info_block[3].text.strip if info_block[3] != nil
+            instance.currency_restrictions_entry = info_block[4].text.strip if info_block[4] != nil
+            instance.currency_restrictions_exit = info_block[5].text.strip if info_block[5] != nil
             i+=1
             countries << instance
       end
       countries.count
     end
 
-  def self.scrape_country(country_url = "https://travel.state.gov/content/travel/en/traveladvisories/traveladvisories/north-korea-travel-advisory.html")
+  def self.scrape_country(info_url = "https://travel.state.gov/content/passports/en/country/cyprus.html")
   blocks = []
-  country_website = Nokogiri::HTML(open(country_url))
+  #country_website = Nokogiri::HTML(open(country_url))
   #info_url = country_website.css("div.tsg-rwd-emergency-alert-text a").attribute("href").value
-  info_url = country_website.at_css('a:contains("country information page")')["href"]
-  if info_url.strip.split("https://travel.state.gov").count == 1
-    info_url = "https://travel.state.gov" + info_url.strip
-  else
-    info_url = info_url.strip
-  end
+#  info_url = country_website.at_css('a:contains("country information page")')["href"]
+#  if info_url.strip.split("https://travel.state.gov").count == 1
+  #  info_url = "https://travel.state.gov" + info_url.strip
+#  else
+  #  info_url = info_url.strip
+  #end
   #binding.pry
   info_website = Nokogiri::HTML(open(info_url))
 #  safety_info = country_website.css("div.tsg-rwd-emergency-alert-text p")
@@ -72,6 +84,7 @@ class TravelSafe::Scraper
   vaccinations = info_block[3].text.strip if info_block[3] != nil
   currency_restrictions_entry = info_block[4].text.strip if info_block[4] != nil
   currency_restrictions_exit = info_block[5].text.strip if info_block[5] != nil
+  binding.pry
 #  safety_block = info_website.css("div.tsg-rwd-accordion-copy")
   #safety_and_security = safety_block[3].text.strip
     #safety_block[3].xpath("p").each do |block|
