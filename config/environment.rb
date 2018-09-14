19d849e97aec475f2d5e3653a37633c0eafe9901
Lauren Pardee <lap5151@gmail.com>
2018-09-14 13:36:12 +0000

    bin/travel_safe revisions

--- config/environment.rb
+++
@@ -1,7 +0,0 @@
-require 'pry'
-require 'nokogiri'
-require 'open-uri'
-
-require_relative './lib/travel_safe/scraper'
-require_relative './lib/travel_safe/country'
-require_relative './lib/travel_safe/cli'
