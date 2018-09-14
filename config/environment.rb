96eae000a051c4d9a18938aed7e2d1e6bd9a1fd9
Lauren Pardee <lap5151@gmail.com>
2018-09-14 02:08:28 +0000

    added config file

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative './lib/travel_safe/scraper'
+require_relative './lib/travel_safe/country'
+require_relative './lib/travel_safe/cli'
